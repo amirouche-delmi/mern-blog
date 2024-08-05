3ca71753847695cac3f049402df32716db6715a0
Amirouche DELMI <amirouche.delmi@gmail.com>
2024-08-05 14:34:10 +0100

    update

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
 
 export const signup = async (req, res) => {
   try {
@@ -15,7 +16,9 @@ export const signup = async (req, res) => {
       return res.status(400).json({ message: "All fields are required" });
     }
 
-    const newUser = new User({ username, email, password });
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+
+    const newUser = new User({ username, email, password: hashedPassword });
 
     await newUser.save();
 
