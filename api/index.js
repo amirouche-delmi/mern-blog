ebc412dcaff4b9ab06e9fa856795e093358ef987
Amirouche DELMI <amirouche.delmi@gmail.com>
2024-08-05 11:17:44 +0100

    update

--- api/index.js
+++ api/index.js
@@ -1,7 +1,28 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config(); // Charger les variables d'environnement depuis le fichier .env
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+// Middleware pour analyser les requêtes JSON
+app.use(express.json());
+
+// Connexion à MongoDB
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MongoDB is connected");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err.message);
+  });
+
+// Définir un port depuis les variables d'environnement ou utiliser 3000 par défaut
+const PORT = process.env.PORT || 3000;
+
+// Démarrer le serveur
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
