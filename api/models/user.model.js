91ea2e401e2f5f667d9880b96def99db25e77f65
Amirouche DELMI <amirouche.delmi@gmail.com>
2024-08-07 14:48:45 +0100

    commit

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -16,10 +16,19 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    profilePicture: {
+      type: String,
+      default:
+        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
   },
   { timestamps: true }
 );
 
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model("User", userSchema);
 
 export default User;
