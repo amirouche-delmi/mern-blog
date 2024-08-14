eeb95b3c8474f1a6af569a09edb81ab61bc18a57
Amirouche DELMI <amirouche.delmi@gmail.com>
2024-08-14 13:00:10 +0100

    add recent article section to the post page

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,16 +2,12 @@ const flowbite = require("flowbite-react/tailwind");
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    flowbite.content(),
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
   theme: {
     extend: {},
   },
   plugins: [
     flowbite.plugin(),
-    require('tailwind-scrollbar'),
+    require("tailwind-scrollbar"),
   ],
-}
+};
